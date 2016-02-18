464799b24530528a2d70d8a5148b38f6c792dcb3
Colin Mathews <colin.mathews@gmail.com>
2016-02-18 17:11:51 -0500

    Scaffolding

---
+++ typings/browser.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="browser/ambient/aws-sdk/aws-sdk.d.ts" />
+/// <reference path="browser/ambient/chai/chai.d.ts" />
+/// <reference path="browser/ambient/es6-promise/es6-promise.d.ts" />
+/// <reference path="browser/ambient/mocha/mocha.d.ts" />
+/// <reference path="browser/ambient/node/node.d.ts" />
+/// <reference path="browser/ambient/source-map-support/source-map-support.d.ts" />
