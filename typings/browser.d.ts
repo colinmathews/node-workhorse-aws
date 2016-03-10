8f62df25a43b95d27b4bb0153b5477f1bcdf5ff3
Colin Mathews <colin.mathews@gmail.com>
2016-03-10 15:36:29 -0500

    Remove typings

--- typings/browser.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="browser/ambient/aws-sdk/aws-sdk.d.ts" />
-/// <reference path="browser/ambient/chai/chai.d.ts" />
-/// <reference path="browser/ambient/es6-promise/es6-promise.d.ts" />
-/// <reference path="browser/ambient/mocha/mocha.d.ts" />
-/// <reference path="browser/ambient/node-uuid/node-uuid.d.ts" />
-/// <reference path="browser/ambient/node/node.d.ts" />
-/// <reference path="browser/ambient/source-map-support/source-map-support.d.ts" />
